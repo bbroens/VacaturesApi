2248da8e12be1585e3b669bfa662cc5f7b5ac499
Ben Broens <bbroens@users.noreply.github.com>
2024-12-05 16:15:11 +0100

    Vacature Model and Dto

--- VacaturesApi/Domain/Vacature.cs
+++ VacaturesApi/Domain/Vacature.cs
@@ -1,6 +1,66 @@
-﻿namespace VacaturesApi.Domain;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VacaturesApi.Domain;
+
+/// <summary>
+/// Model class representing a single Vacature.
+/// This class is used by Entity Framework to create and manipulate this table in the DB.
+/// By modifying this class you will modify the database using migrations.
+/// </summary>
 
 public class Vacature
 {
+	[Column("VacatureId")]
+    public Guid Id { get; set; }
     
+	[Required(ErrorMessage = "UrlSlug is a required field")]
+	[MaxLength(256, ErrorMessage = "UrlSlug cannot be longer than 256 characters")]
+    public string UrlSlug { get; set; } = string.Empty;
+    
+	[Required(ErrorMessage = "Function is a required field")]
+	[MaxLength(256, ErrorMessage = "Function cannot be longer than 256 characters")]
+    public string Function { get; set; } = string.Empty;
+    
+	[Required(ErrorMessage = "Availability is a required field")]
+	[MaxLength(64, ErrorMessage = "Availability cannot be longer than 64 characters")]
+    public string Availability { get; set; } = string.Empty;
+    
+	[Required(ErrorMessage = "Location is a required field")]
+	[MaxLength(128, ErrorMessage = "Location cannot be longer than 128 characters")]
+    public string Location { get; set; } = string.Empty;
+    
+	[Required(ErrorMessage = "ContactPerson is a required field")]
+	[MaxLength(64, ErrorMessage = "ContactPerson cannot be longer than 64 characters")]
+    public string ContactPerson { get; set; } = string.Empty;
+    
+	[Required(ErrorMessage = "Description is a required field")]
+	public string Description { get; set; } = string.Empty;
+    
+	[Required(ErrorMessage = "WhatToExpect is a required field")]
+	public string WhatToExpect { get; set; } = string.Empty;
+    
+	[Required(ErrorMessage = "TaskDescription is a required field")]
+	public string TaskDescription { get; set; } = string.Empty;
+    
+	[Required(ErrorMessage = "Offer is a required field")]
+	public string Offer { get; set; } = string.Empty;
+    
+	[Required(ErrorMessage = "Requirements is a required field")]
+	public string Requirements { get; set; } = string.Empty;
+    
+	[MaxLength(32, ErrorMessage = "SalaryRange cannot be longer than 32 characters")]
+	public string? SalaryRange { get; set; } = string.Empty;
+    
+	[MaxLength(128, ErrorMessage = "Industry cannot be longer than 128 characters")]
+    public string? Industry { get; set; } = string.Empty;
+    
+	[MaxLength(3, ErrorMessage = "ListPriority cannot be longer than 3 digits")]
+    public int? ListPriority { get; set; } = 0;
+    
+    public bool? Hidden { get; set; } = false;
+    
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
 }
--- VacaturesApi/Features/Vacatures/VacatureDto.cs
+++ VacaturesApi/Features/Vacatures/VacatureDto.cs
@@ -1,6 +1,27 @@
 ﻿namespace VacaturesApi.Features.Vacatures;
 
-public class VacatureDto
-{
-    
-}
+/// <summary>
+/// Data Transfer Object for a Vacature.
+/// By using a record we can compare actual values instead of object references.
+/// This is more performant and avoids unnecessary allocations and equals checks.
+/// </summary>
+
+public record VacatureDto(
+    Guid Id,
+    string UrlSlug,
+    string Function,  
+    string Availability,  
+    string Location,  
+    string ContactPerson,  
+	string Description,  
+	string WhatToExpect,  
+	string TaskDescription,  
+	string Offer,  
+	string Requirements,  
+	string SalaryRange,  
+    string Industry,  
+    int ListPriority,
+    bool Hidden,
+    DateTime CreatedAt, 
+    DateTime? UpdatedAt 
+);
