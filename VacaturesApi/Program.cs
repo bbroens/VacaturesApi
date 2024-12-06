ef8101e2749341e78eb04bac1bd87f694a267548
Ben Broens <bbroens@users.noreply.github.com>
2024-12-06 12:38:30 +0100

    Program.cs cleanup

--- VacaturesApi/Program.cs
+++ VacaturesApi/Program.cs
@@ -7,24 +7,22 @@ using VacaturesApi.Common.Interfaces;
 using VacaturesApi.Features.Vacatures;
 using VacaturesApi.Persistence.Seeding;
 
-// Recommended by Serilog:
-// The initial "bootstrap" logger is able to log errors during start-up. It's replaced by the logger
-// configured in `AddSerilog()` below, once config and dependency-injection have been set up successfully.
+// The initial bootstrap logger is able to log errors during start-up.
+// On successful startup it is replaced by the logger configured in AddSerilog().
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
     .CreateBootstrapLogger();
 
 try
 {
-    Log.Information("Starting web application");
-
-    // ##################################################
+    Log.Information("Starting vacatures api...");
+    
     // Add services to the build container.
-    // ##################################################
-        
+    // ######################################
+    
     var builder = WebApplication.CreateBuilder(args);
 
-    // Register and config serilog to use logging
+    // Register and config serilog
     builder.Services.AddSerilog((services, config) => config
         .ReadFrom.Configuration(builder.Configuration)
         .ReadFrom.Services(services)
@@ -32,13 +30,14 @@ try
         .WriteTo.Console()
         .WriteTo.File("Logs/logs-vacaturesapi-.txt", rollingInterval: RollingInterval.Day));    
 
+    // Add services for controllers
     builder.Services.AddControllers();
 
-    // Configure Entity Framework with SQL Server
+    // Configure DbContext
     builder.Services.AddDbContext<VacatureDbContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("VacatureDbConnection")));
 
-    // Register automapper
+    // Register Automapper
     builder.Services.AddAutoMapper(typeof(Program).Assembly);
     
     // Register MediatR
@@ -47,16 +46,16 @@ try
 
     // Register repositories
     builder.Services.AddScoped<IVacatureRepository, VacatureRepository>();
-
-    // ##################################################
+    
+    
     // Configure the HTTP request pipeline.
-    // ##################################################
-
+    // #####################################
+    
     var app = builder.Build();
 
     if (app.Environment.IsDevelopment())
     {
-        // Seed dev database if it's empty
+        // Seed development Db once if empty
         using var scope = app.Services.CreateScope();
         var dbContext = scope.ServiceProvider.GetRequiredService<VacatureDbContext>();
         DataSeeder.Seed(dbContext);
@@ -64,27 +63,27 @@ try
     
     // Add global exception handling middleware
     app.UseMiddleware<GlobalExceptionHandler>();
-
-    // Configure the HTTP request pipeline
+    
     if (app.Environment.IsProduction())
         app.UseHsts();
-
+    
     app.UseAuthorization();
 
     app.MapControllers();
 
     app.Run();
 
+    // On application exit
     Log.Information("Web application stopped cleanly");
-    
     return 0;
 }
 catch (Exception ex)
 {
-    Log.Fatal(ex, "An unhandled exception occurred during logger bootstrapping");
+    Log.Fatal(ex, "The application failed to start correctly.");
     return 1;
 }
 finally
 {
+    // Write Serilog events to sinks before exit
     Log.CloseAndFlush();
 }
